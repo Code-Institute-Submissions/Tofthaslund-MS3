df1870b32791fc702d3282daddcfca3633a91d5c
Lasse Haslund <tofthaslund@gmail.com>
2021-07-21 13:41:25 +0100

    connected Sign In to db

--- app.py
+++ app.py
@@ -48,9 +48,32 @@ def signup():
     return render_template("signup.html")
 
 
-@app.route("/signin")
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
-    return render_template("signin.html")
+    if request.method == "POST":
+        # check if username exists in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+    
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}".format(request.form.get("username")))
+                    return redirect(url_for("profile", username=session["user"])) 
+            else:
+                # incorrect password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("Signin"))
+        else:
+            # Username is incorrect
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("signin"))
+
+    return  render_template("signin.html")
+    
+
 
 
 if __name__ == "__main__":
