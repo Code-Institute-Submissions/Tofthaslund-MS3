639fcbf2345e6e8e8969c4ba0ccdbab1bad71f59
Lasse Haslund <tofthaslund@gmail.com>
2021-07-30 12:59:59 +0100

    imported collapse bars from bootstrap to show tasks on tasks.html

--- app.py
+++ app.py
@@ -97,6 +97,12 @@ def logout():
     return redirect(url_for("signin"))
 
 
+@app.route("/tasks")
+def tasks():
+    tasks = list(mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=tasks)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port = int(os.environ.get("PORT")),
